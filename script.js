e35f0bb0e57884788c7dcfa4e2942562eb386f73
Jonahwk <jonah@wadeskog.com>
2024-03-28 16:22:27 +0100

    nästa commit

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let message = '';
 const random = Math.floor(Math.random()*3) +1;
 
 console.log(random);
+
+
+function slumpen(num) {
+    if (random === 1) {
+        return('Hejhej jag är först');
+    } else if (random === 2) {
+        return('Jag ör tvåa');
+    } else {
+        return('jag ör treeeeeeeeeeeaaaaaaaa');
+    }
+}
+
+console.log(slumpen(random));
